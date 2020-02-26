363853c8b553d47b7811d0f48d5930d7fb5273dd
Ivan Escobar Chapa <iaech1999@hotmail.com>
2020-02-26 08:30:02 -0500

    Hasta aqui llegue

---
+++ Controllers/Agregar_VideoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPlantilla.Controllers
+{
+    public class Agregar_VideoController : Controller
+    {
+        //
+        // GET: /Agregar_Video/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
